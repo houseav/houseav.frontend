366baa6f6ceae55a2120daf47629dbdec154f6d9
Luca Imbalzano <lucaimbalzano@gmail.com>
2024-08-29 19:53:19 +0200

    understand if BASE_API_URL is right

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   const BASE_API_URL = `${env.VITE_ENDPOINT_API_V1 ?? "http://localhost:3000"}`;
-
+  console.log("BASE_API_URL", BASE_API_URL);
   return {
     server: {
       origin: BASE_API_URL,
